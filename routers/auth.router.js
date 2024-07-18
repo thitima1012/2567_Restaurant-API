ac409b93a32543a3776de8eb353e4c0734e3d280
thitima1012 <thitima1235pim@gmail.com>
2024-07-18 11:44:53 +0700

    middleware

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controlles/auth.controller");
+const {verifySignUp} = require("../middlewares");
 
-router.post("/signup", authController.signup);
+router.use((req, res, next)=>{
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});
+
+router.post(
+    "/signup",
+    [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+    authController.signup
+);
+
 router.post("/signin", authController.signin);
+
+//router.post("/signup", authController.signup);
+//router.post("/signin", authController.signin);
 
 module.exports = router;
